c656b29eddb2192b97206354e60c3d46bf0f5ed9
Emekaugo <36327185+Emekaugo@users.noreply.github.com>
2024-02-28 10:39:19 +0100

    backend

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,6 @@
 export const PORT = 5555;
 
+// export const mongoDBURL ="mongodb+srv://microemeka:megaman1998@clusterdb.erphwhi.mongodb.net/books-collection?retryWrites=true&w=majority";
+
 export const mongoDBURL =
-  "mongodb+srv://microemeka:megaman1998@clusterdb.erphwhi.mongodb.net/books-collection?retryWrites=true&w=majority";
+  "mongodb+srv://microemeka2:megaman1998@clusterdb.erphwhi.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Clusterdb";
